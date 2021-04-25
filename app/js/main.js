17184c75c0443535751db1068caba4ca7fcef0f7
sergk1993 <81139100+sergk1993@users.noreply.github.com>
2021-04-25 17:15:56 +0600

    media about

--- app/js/main.js
+++ app/js/main.js
@@ -5,6 +5,32 @@ $('.about__slider').slick({
     slidesToScroll:1,
     prevArrow:' <button class="about__arrow about__arrow-prev"><img src="images/about-arrow-prev.svg" alt=""></button>',
     nextArrow:' <button class="about__arrow about__arrow-next"><img src="images/about-arrow-next.svg" alt=""></button>',
+    responsive: [
+        {
+          breakpoint: 1000,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            
+          }
+        },
+        {
+            breakpoint: 700,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              
+            }
+          },
+          {
+            breakpoint: 500,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              
+            }
+          },
+    ]
 });
 
 var mixer = mixitup('.works__inner');
