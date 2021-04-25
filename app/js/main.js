23e3fde5f7148b1069af98ce28ed958db4213c9d
sergk1993 <81139100+sergk1993@users.noreply.github.com>
2021-04-25 11:33:10 +0600

    add about

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,11 @@
 $(function(){
 
-
+$('.about__slider').slick({
+    slidesToShow:4,
+    slidesToScroll:1,
+    prevArrow:' <button class="about__arrow about__arrow-prev"><img src="images/about-arrow-prev.svg" alt=""></button>',
+    nextArrow:' <button class="about__arrow about__arrow-next"><img src="images/about-arrow-next.svg" alt=""></button>',
+});
 
   
 });
